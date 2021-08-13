927944aa5492f761be28ffb13c9e9f44eb6347d8
Ishaan3030 <86636860+Ishaan3030@users.noreply.github.com>
2021-08-13 09:51:00 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,6 +2,13 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+let engine;
+let world;
+var canvas, base, bg;
+
+function preload(){
+   bg = loadImage();
+}
 
 function setup() {
   canvas = createCanvas(1200,600);
